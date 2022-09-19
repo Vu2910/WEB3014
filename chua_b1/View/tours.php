9d788da4a0d81d12d02f863de739dba68da48e6e
Vu2910 <ximaxi2022@gmail.com>
2022-09-19 10:17:21 +0700

    ADD, DELETE, UPDATE

---
+++ chua_b1/View/tours.php
@@ -0,0 +1,27 @@
+<?php
+    //Đã có biến $tours từ controller trước khi include view
+?>
+<h2><a href="./index.php?url=create-tour">Create Tour</a></h2>
+    <thead>
+        <tr>
+            <td>ID</td>
+            <td>Name</td>
+            <td>Category</td>
+            <td>Edit</td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($tours as $key => $ts) : ?>
+            <tr>
+                <td><?= $ts['id'] ?></td>
+                <td><?= $ts['name'] ?></td>
+                <td><?= $ts['cate_name'] ?></td>
+                <td>
+                    <a href="./index.php?url=edit-tour&id=<?= $ts['id'] ?>">Update</a>
+                    /
+                    <a onclick="return confirm('Bạn có chắc muốn xóa không?')" href="./index.php?url=destroy-tour&id=<?= $ts['id'] ?>">Delete</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
